5278c94bc1297aa5e85f6a754dc47b1052e90fae
Bora Çakmak <Bora1.cakmak@hotmail.com>
2026-06-14 00:29:54 +0300

    Update trainmodel.py

--- trainmodel.py
+++ trainmodel.py
@@ -1,10 +1,18 @@
 from ultralytics import YOLO
 import torch
 
-model = YOLO('yolo11n.pt') 
+model = YOLO('yolo11n.pt')
 
 if __name__ == '__main__':
     if torch.cuda.is_available():
-        print(f"Success! Using GPU: {torch.cuda.get_device_name(0)}")
-    
-    results = model.train(data='data.yaml', epochs=50, imgsz=640, device=0)
+        print(f"Using GPU: {torch.cuda.get_device_name(0)}")
+    else:
+        print("No GPU Found.")
+
+    results = model.train(
+        data='data.yaml',
+        epochs=100,
+        imgsz=640,
+        device=0,
+        patience=15,
+    )
